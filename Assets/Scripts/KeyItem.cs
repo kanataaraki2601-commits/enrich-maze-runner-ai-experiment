3ac4d37516af316d98b3e91db3d71798062a8b3e
kanataaraki2601-commits <kanataaraki2601@gmail.com>
2025-11-08 13:01:30 +0900

    Add KeyItem class to handle kAdd KeyItem script for key collectioney collection

---
+++ Assets/Scripts/KeyItem.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class KeyItem : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            GameManager.Instance.hasKey = true;
+            Destroy(gameObject);
+        }
+    }
+}
